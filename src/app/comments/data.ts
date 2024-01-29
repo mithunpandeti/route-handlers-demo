c74eb445cad6d154dbaf78587db5feb1e2bf68c1
Pandeti Mithun Kumar <mithunpandeti@gmail.com>
2024-01-29 18:22:40 -0500

    Handling Get Request - We have used thunder client extension in vs code to call GET API http://localhost:3000/comments

---
+++ src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        text: "This is Comment One"
+    },
+    {
+        id: 2,
+        text: "This is Comment Two"
+    },
+    {
+        id: 3,
+        text: "This is Comment Three"
+    }
+]
