ff1f8951577368bc22e6894dd8e39bdaf5b4d3df
Pandeti Mithun Kumar <mithunpandeti@gmail.com>
2024-01-29 18:53:51 -0500

    PATCH request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -4,3 +4,11 @@ export async function GET(request: Request, { params }: { params: { id: string}}
     const comment = comments.find((comment) => comment.id === parseInt(params.id));
     return Response.json(comment)
 }
+
+export async function PATCH(request: Request, { params }: { params: {id: string}}) {
+    const comment = await request.json();
+    const { text } = comment;
+    const findIndex = comments.findIndex(comment => comment.id === parseInt(params.id));
+    comments[findIndex] = comment;
+    return Response.json(comments[findIndex])
+}
