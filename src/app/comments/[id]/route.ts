a104e83d462515032decd221090a3d7609af940c
Pandeti Mithun Kumar <mithunpandeti@gmail.com>
2024-01-30 23:37:01 -0500

    Redirect in Route Handlers

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,6 +1,8 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(request: Request, { params }: { params: { id: string}}){
+    if(parseInt(params.id) > comments.length){redirect("/comments")}// If the data is not present, redirect to respected page
     const comment = comments.find((comment) => comment.id === parseInt(params.id));
     return Response.json(comment)
 }
