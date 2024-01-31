2d87baa85842760417562136702c68d93d6e2cae
Pandeti Mithun Kumar <mithunpandeti@gmail.com>
2024-01-30 23:56:33 -0500

    Setting Headers in Request and Response

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET(){
-    return new Response("Hello Profile!");
+import { type NextRequest } from "next/server"; 
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest){
+
+    const requestHeaders = new Headers(request.headers);//first approach to get headers data using Nest request
+    const headerList = headers();//second approach to get headers data using headers
+
+    // We are fetching the Authorization header
+    console.log(requestHeaders.get('Authorization'));
+    console.log(headerList.get('Authorization'))
+    return new Response("{name: 'Mithun', address: 'New York'}",{
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
 }
